28557ca3611ceae16369f0be439a7eaf78a46ff9
Tristan Andrus <tristan@tristanandrus.com>
2025-10-20 12:09:12 -0600

    Fix support for Debian-based distros

--- distros/debian/distrosupport.sh
+++ distros/debian/distrosupport.sh
@@ -6,10 +6,22 @@ function confound_package_update_upgrade() {
     sudo apt-get upgrade -y
 }
 
-function confound_package_install() {
+function confound_debian_package_install() {
     # TODO: Translate through distro_packages array
     # Arguments: all are passed in as confound package names
     declare -a packages
     packages=( $(confound_print_converted_package_names "$*" ) )
     sudo apt-get install "${packages[@]}" -y
+}
+
+# The easisest thing to do is just define and pass all the debian-derivatives
+# to the debian package install function.
+function confound_ubuntu_package_install() {
+    confound_debian_package_install $@
+}
+function confound_pop_package_install() {
+    confound_debian_package_install $@
+}
+function confound_mint_package_install() {
+    confound_debian_package_install $@
 }
